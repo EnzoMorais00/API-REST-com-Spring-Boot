b3ce3f6fa74eaa5f42fb20cc8e6832c51b4a812f
Enzo Morais <139204929+EnzoMorais00@users.noreply.github.com>
2026-09-13 16:18:13 -0300

    Adiciona inicialização da aplicação

---
+++ src/main/java/com/exemplo/apialunos/ApiAlunosApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo.apialunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlunosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAlunosApplication.class, args);
+    }
+}
